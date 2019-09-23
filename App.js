fa87bbfc511b34aaa7ab03d983686b79f7a7f56b
faris114 <farisabutaya@gmail.com>
2019-09-23 16:08:03 +0300

    footer added

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import {View} from 'react-native';
 import React from 'react';
 import Header from './src/Components/Header/header.js';
 import AppContainer from './src/Routes.js';
+import Footer from './src/Components/footer/footer';
 export default class App extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
         <Header />
         <AppContainer />
+        <Footer />
       </View>
     );
   }
